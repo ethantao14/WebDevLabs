ff379bf92eb9c99ed37fa6b914b875c77c670ce7
ethantao14 <156117320+ethantao14@users.noreply.github.com>
2025-03-23 22:06:53 -0400

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,39 +1,39 @@
-var x = 5;
-var y = 7;
-var z = x + y;
-console.log(z);
+// var x = 5;
+// var y = 7;
+// var z = x + y;
+// console.log(z);
 
-var A = "Hello ";
-var B = "world!";
-var C = A + B;
-console.log(C);
+// var A = "Hello ";
+// var B = "world!";
+// var C = A + B;
+// console.log(C);
 
-function SumNPrint(x1, x2)
-{
-    x3 = x1 + x2;
-    console.log(x3);
-}
+// function SumNPrint(x1, x2)
+// {
+//     x3 = x1 + x2;
+//     console.log(x3);
+// }
 
-SumNPrint(x, y);
-SumNPrint(A, B);
+// SumNPrint(x, y);
+// SumNPrint(A, B);
 
-if(C.length > z)
-{
-    console.log(C);
-}
+// if(C.length > z)
+// {
+//     console.log(C);
+// }
 
-else if(C.length < z)
-{
-    console.log(z);
-}
+// else if(C.length < z)
+// {
+//     console.log(z);
+// }
 
-else if(C.length === z)
-{
-    console.log("good job!");
-}
+// else if(C.length === z)
+// {
+//     console.log("good job!");
+// }
 
-var L1 = ["Watermelon", "Pineapple", "Pear", "Banana"];
-var L2 = ["Apple", "Banana", "Kiwi", "Orange"];
+// var L1 = ["Watermelon", "Pineapple", "Pear", "Banana"];
+// var L2 = ["Apple", "Banana", "Kiwi", "Orange"];
 
 /*
 function findTheBanana(arr)
@@ -50,7 +50,7 @@ function findTheBanana(arr)
 findTheBanana(L1);
 findTheBanana(L2);
 */
-
+/*
 function findTheBanana(str)
 {
     if(str === "Banana")
@@ -59,12 +59,15 @@ function findTheBanana(str)
         return;
     }
 }
-
+*/
 //L1.forEach(findTheBanana);
 //L2.forEach(findTheBanana);
 
+
+
 function greetingFunc()
 {
+    if (window.location.pathname.indexOf('/index.html') > -1) {
     var d = new Date();
     var h;
     h = d.getHours();
@@ -85,6 +88,7 @@ function greetingFunc()
         //console.log("Good night");
         document.getElementById("time").innerHTML 
         = "Good night, I am Ethan Tao";
+    }
 }
 
 greetingFunc();
@@ -97,54 +101,60 @@ function addYear()
     document.getElementById("copyYear").innerHTML += " " + y;
 }
 
-function showList()
+// function showList()
+// {
+//     document.getElementById("hobbiesList").style.display = "block";
+//     document.getElementById("viewButton").style.display = "none";
+// }
+
+if (window.location.pathname.indexOf('/index.html') > -1) 
 {
-    document.getElementById("hobbiesList").style.display = "block";
-    document.getElementById("viewButton").style.display = "none";
+    $(document).ready(function(){
+        $("#long-bio, #read-less").hide();
+    
+        $("#read-more").click(function(){
+        $("#long-bio").slideDown();
+        $("#read-more").hide();
+        $("#read-less").show();
+        });
+    
+        $("#read-less").click(function(){
+        $("#long-bio").slideUp();
+        $("#read-less").hide();
+        $("#read-more").show();
+        });
+    });
 }
-
-$(document).ready(function(){
-    $("#long-bio, #read-less").hide();
-  
-    $("#read-more").click(function(){
-      $("#long-bio").slideDown();
-      $("#read-more").hide();
-      $("#read-less").show();
-    });
-  
-    $("#read-less").click(function(){
-      $("#long-bio").slideUp();
-      $("#read-less").hide();
-      $("#read-more").show();
-    });
-  });
   
 
-  document.addEventListener("DOMContentLoaded", function() {
+function validate()
+{
+    console.log("in event listener function");
     const nameInput = document.getElementById("name");
     const emailInput = document.getElementById("email");
     const messageInput = document.getElementById("comment");
     const form = document.getElementById("contactForm");
-    function validateName() {
-      if (!nameInput.checkValidity()) {
-        console.log("Please enter your name.");
-        return false;
-      }
-      return true;
+    if (!nameInput.checkValidity()) 
+    {
+        document.getElementById("feedback").innerHTML = "Please enter a valid name.";
     }
-    function validateEmail() {
-      if (!emailInput.checkValidity()) {
-        //console.log("Please enter a valid email address.");
+    if (!emailInput.checkValidity()) 
+    {
         document.getElementById("feedback").innerHTML = "Please enter a valid email address.";
-        return false;
-      }
-      return true;
     }
-    function validateMessage() {
-      if (!messageInput.checkValidity()) {
-        console.log("Please enter your message.");
-        return false;
-      }
-      return true;
+    if (!messageInput.checkValidity()) 
+    {
+        document.getElementById("feedback").innerHTML = "Please enter a valid comment.";
     }
-})
+}
+
+function getAdvice()
+{
+    fetch('https://api.adviceslip.com/advice')
+      .then(response => response.json())
+      .then(data => {
+        const advice = data.slip.advice;
+        document.getElementById('adviceText').innerText = advice;
+      })
+      .catch(error => console.error('Error fetching advice:', error));
+}
